6e803ac1f27bf975f6d909820a6d0504281a084e
korashy <a.ashraf.elgammal@hotmail.com>
2023-09-10 15:08:51 +0300

    fix the import of model user

--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => \App\Domains\User\Models\User::class,
+            'model' => \App\Models\User::class,
         ],
 
         // 'users' => [
